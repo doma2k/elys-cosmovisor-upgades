a642975f54694508510565ed1467065cc1a1856f
doma2k <domakyrylo@gmail.com>
2023-06-04 11:49:43 +0000

    updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # elys-cosmovisor-upgades
+
+- comparing latest binary version to existing upgrades cosmovisor folder 
+- if latest binary doesnt not exists it creates new cosmovisor folder and building new binary
+- !!! works only with latest upgrades if two or more upgrades missing , might not work
--- app.js
+++ app.js
@@ -1,63 +1,41 @@
 const axios = require('axios');
 const fs = require('fs');
-const download = require('download');
-const extract = require('extract-tar');
 const { execSync } = require('child_process');
 
 const GITHUB_REPO = 'elys-network/elys';
-const ELYS_UPGRADES = `${process.env.HOME}/.elys/cosmovisor/upgrades`;
+const UPGRADES = `${process.env.HOME}/.elys/cosmovisor/upgrades`;
+const BINARY_NAME = 'elysd';
 
 async function getLatestRelease() {
     const response = await axios.get(`https://api.github.com/repos/${GITHUB_REPO}/releases`);
     const version = response.data[0];
-    console.log(version.tag_name);
+    console.log('Latest binary:', version.tag_name);
     return version.tag_name;
 }
 
 async function main() {
-    const version = await getLatestRelease();
 
-    const directories = fs.readdirSync(ELYS_UPGRADES);
+    const version = await getLatestRelease();
+    const directories = fs.readdirSync(UPGRADES);
+
     if (!directories.includes(version)) {
         console.log(`New version found: ${version}`);
-        // Your process to download and compile the new version here...
+
+        const newVersionPath = `${UPGRADES}/${version}/bin/`;
+        fs.mkdirSync(newVersionPath, { recursive: true });
+        console.log(`Created new directory: ${newVersionPath}`);
+
+        // Building the new version
+        try {
+            console.log("Building new version...");
+            execSync(`cd ${process.env.HOME}/elys && git clean -fd && git reset --hard && git fetch --all && git checkout ${version} && make install && mv ${process.env.HOME}/go/bin/${BINARY_NAME} ${newVersionPath}`, { stdio: 'inherit' });
+            console.log("Build complete.");
+        } catch (error) {
+            console.error("Error during build:", error);
+        }
     } else {
         console.log("No new version found.");
     }
 }
 
-main()
-
-// async function downloadAndExtract(url, version) {
-//     await download(url, '.', { filename: `${version}.tar.gz` });
-//     await extract({ file: `${version}.tar.gz`, cwd: '.' });
-// }
-
-// function compileCode() {
-//     // replace this command with your build commands
-//     let commands = [
-//         'cd project_directory',
-//         'make'
-//     ];
-
-//     commands.forEach((command) => {
-//         execSync(command, { stdio: 'inherit' });
-//     });
-// }
-
-// async function main() {
-//     const release = await getLatestRelease();
-//     const version = release.tag_name;
-//     const tarballUrl = release.tarball_url;
-
-//     if (!fs.existsSync(`./${version}`)) {
-//         console.log(`New version found: ${version}. Downloading and extracting...`);
-//         await downloadAndExtract(tarballUrl, version);
-//         console.log("Compiling new version...");
-//         compileCode();
-//     } else {
-//         console.log("No new version found.");
-//     }
-// }
-
-// main();
+main();
