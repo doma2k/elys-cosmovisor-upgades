074443a6074986a67587b6ebae9aabc853ad0705
doma2k <79820904+doma2k@users.noreply.github.com>
2023-09-07 12:53:33 +0100

    sh upgarde

--- cosmovisor-upgrade.sh
+++ cosmovisor-upgrade.sh
@@ -2,8 +2,9 @@
 
 # Configuration
 GITHUB_REPO="elys-network/elys"
-UPGRADES_PATH="/path/to/your/upgrades"
+UPGRADES_PATH="$HOME/path/to/your/upgrades"  # Use $HOME for the home directory
 BINARY_NAME="elysd"
+ELYSD_DIRECTORY="$HOME/elys"  # Use $HOME for the home directory
 
 get_latest_release() {
     local version
@@ -20,18 +21,27 @@ create_directory_for_version() {
     echo "$new_version_path"
 }
 
+download_repository_if_not_exists() {
+    if [ ! -d "$ELYSD_DIRECTORY" ]; then
+        echo "Downloading Elysd repository..."
+        git clone "https://github.com/$GITHUB_REPO.git" "$ELYSD_DIRECTORY"
+    else
+        echo "Elysd repository already exists."
+    fi
+}
+
 build_new_version() {
     local version="$1"
     local new_version_path="$2"
     echo "Building new version..."
     (
-        cd /root/elys || exit 1
+        cd "$ELYSD_DIRECTORY" || exit 1
         git clean -fd
         git reset --hard
         git fetch --all
         git checkout "$version"
         make install
-        mv /root/go/bin/"$BINARY_NAME" "$new_version_path"
+        mv "$HOME/go/bin/$BINARY_NAME" "$new_version_path"  # Use $HOME for the home directory
     ) &> build.log
     if [ $? -eq 0 ]; then
         echo "Build complete"
@@ -45,6 +55,8 @@ main() {
     local version
     version=$(get_latest_release)
     directories=("$UPGRADES_PATH"/*)
+
+    download_repository_if_not_exists
 
     if ! [[ " ${directories[@]} " =~ " $UPGRADES_PATH/$version " ]]; then
         echo "New version found: $version"
